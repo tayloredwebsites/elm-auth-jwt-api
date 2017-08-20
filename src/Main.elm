6f5ef513ad907c600cb84e1ead4809759d9ed780
Dave Taylor of Taylored Web Sites <tayloredwebsites@me.com>
2017-08-20 18:37:09 -0400

    Initial commit with basic model, update & view, no api.

---
+++ src/Main.elm
@@ -0,0 +1,70 @@
+port module Main exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
+
+
+
+{-
+    MODEL
+    * Model type
+    * Initialize model with empty values
+-}
+
+
+type alias Model =
+    { quote : String
+    }
+
+init : (Model, Cmd Msg)
+init =
+    ( Model "", Cmd.none )
+
+
+
+{-
+   UPDATE
+   * Messages
+   * Update case
+-}
+
+
+type Msg = GetQuote
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GetQuote ->
+            ( { model | quote = model.quote ++ "Appended this." }, Cmd.none )
+
+
+
+{-
+   VIEW
+   * Get a quote
+-}
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ] [
+        h2 [ class "text-center" ] [ text "Chuck Norris Quotes" ]
+        , p [ class "text-center" ] [
+            button [ class "btn btn-success", onClick GetQuote ] [ text "Grab a quote!" ]
+        ]
+        -- Blockquote with quote
+        , blockquote [] [
+            p [] [text model.quote]
+        ]
+    ]
